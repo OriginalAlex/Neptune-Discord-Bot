9abe34186b619fdd2c5c4139a713e65ee4ce1ae1
OriginalAlex <ajcartwright.alex288+1@gmail.com>
2017-08-29 21:02:37 +0100

    Added game playing and clipboard function

--- src/main/java/com/originalalex/github/functionalities/Clipboard.java
+++ src/main/java/com/originalalex/github/functionalities/Clipboard.java
@@ -1,5 +1,7 @@
 package com.originalalex.github.functionalities;
 
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 import java.awt.*;
@@ -13,7 +15,11 @@ public class Clipboard implements Function {
         try {
             String data = (String) Toolkit.getDefaultToolkit()
                     .getSystemClipboard().getData(DataFlavor.stringFlavor); // Copy and pasted code from SO
-            e.getChannel().sendMessage(data).queue(); // Send what is on your clipboard
+            MessageEmbed embed = new EmbedBuilder()
+                    .setTitle(data)
+                    .setColor(Color.CYAN)
+                    .build();
+            e.getChannel().sendMessage(embed).queue(); // Send what is on your clipboard
         } catch (Exception ex) {
             ex.printStackTrace();
         }
