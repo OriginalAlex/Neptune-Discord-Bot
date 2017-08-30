ac0c2d83c5eb40c94b1fcd6eda1a7bdb9d453be5
OriginalAlex <ajcartwright.alex288+1@gmail.com>
2017-08-30 23:44:36 +0100

    Changed Main slightly

--- src/main/java/com/originalalex/github/Main.java
+++ src/main/java/com/originalalex/github/Main.java
@@ -1,18 +1,16 @@
 package com.originalalex.github;
 
-import com.originalalex.github.functionalities.BeautifulPic;
 import com.originalalex.github.listener.MessageListener;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.Guild;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            JDA jda = new JDABuilder(AccountType.CLIENT)
-                    .setToken("MjY0MzU1MTEzNTAwNDA5ODU2.C0fXdQ.FUsDvDIIiVt_gn2ZMWLfrEEN2BI")
+            JDA jda = new JDABuilder(AccountType.BOT)
+                    .setToken("MzUyNTczODI4OTc2NzM4MzA2.DIjIQg.iph7Pq93n00TiFTRTmfS-bmmp4Q")
                     .buildBlocking();
             jda.addEventListener(new MessageListener(jda));
         } catch (Exception e) {
