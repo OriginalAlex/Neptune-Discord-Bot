ce12696eca40fef587f7e2b6d1c05ce2293c6c5e
awesome90 <awesome999888@outlook.com>
2017-09-03 17:18:39 +0100

    Fixed a few markdown issues with the help command

--- src/main/java/com/originalalex/github/functionalities/Help.java
+++ src/main/java/com/originalalex/github/functionalities/Help.java
@@ -10,73 +10,82 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class Help implements Function {
 
-    // Section of README.md to read from.
-    private static final String SECTION = "Current Commands:";
+	// Section of README.md to read from.
+	private static final String SECTION = "Current Commands:";
 
-    private String message;
-    private List<String> sendOrder;
+	private String message;
+	private List<String> sendOrder;
 
-    public Help() {
-        // Can we read from the cache?
+	public Help() {
+		// Can we read from the cache?
 
-        // Get README.md (use the raw text file)
-        URL url;
-        try {
-            url = new URL("https://raw.githubusercontent.com/OriginalAlex/DiscordBot/master/README.md");
-            Scanner scanner = new Scanner(url.openStream());
+		// Get README.md (use the raw text file)
+		URL url;
+		try {
+			url = new URL("https://raw.githubusercontent.com/OriginalAlex/DiscordBot/master/README.md");
+			Scanner scanner = new Scanner(url.openStream());
 
-            StringBuilder sb = new StringBuilder();
+			StringBuilder sb = new StringBuilder();
 
-            boolean read = false;
+			boolean read = false;
 
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                // Read from the "Current Commands" section
-                if (!read) {
-                    if (line.contains(SECTION)) {
-                        read = true;
-                    } else {
-                        continue;
-                    }
-                }
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine();
+				// Read from the "Current Commands" section
+				if (!read) {
+					if (line.contains(SECTION)) {
+						read = true;
+					} else {
+						continue;
+					}
+				}
 
-                // Read for the next section so we don't pull more than we need
-                // to
-                if (line.contains("##") && !line.contains(SECTION)) {
-                    read = false;
-                    break;
-                }
+				// Read for the next section so we don't pull more than we need
+				// to
+				if (line.contains("##") && !line.contains(SECTION)) {
+					read = false;
+					break;
+				}
 
-                // Otherwise, add to the output text
-                sb.append(line + "\n");
-            }
-            message = sb.toString();
-            this.sendOrder = divideStringIntoSubStringsOf2000Chars(message);
-            scanner.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+				// Otherwise, add to the output text
+				sb.append(line + "\n");
+			}
+			message = sb.toString();
 
-    private List<String> divideStringIntoSubStringsOf2000Chars(String str) { // the messages must be < 2000 characters
-        List<String> messages = new ArrayList<>();
-        String temp = "";
-        String[] lines = str.split("\n");
-        for (String line : lines) {
-            if (temp.length() + line.length() >= 2000) {
-                messages.add(temp);
-                temp = "";
-            }
-            temp += line;
-            temp += "\n";
-        }
-        messages.add(temp.substring(0, temp.lastIndexOf("\n")));
-        return messages;
-    }
+			message = message.replace("##", "").trim();
 
-    @Override
-    public void handle(MessageReceivedEvent e, String[] parts) {
-        e.getAuthor().openPrivateChannel().queue(channel -> sendOrder.forEach(m -> channel.sendMessage(m).queue()));
-    }
+			this.sendOrder = divideStringIntoSubStringsOf2000Chars(message);
+			scanner.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private List<String> divideStringIntoSubStringsOf2000Chars(String str) { // the
+																				// messages
+																				// must
+																				// be
+																				// <
+																				// 2000
+																				// characters
+		List<String> messages = new ArrayList<>();
+		String temp = "";
+		String[] lines = str.split("\n");
+		for (String line : lines) {
+			if (temp.length() + line.length() >= 2000) {
+				messages.add(temp);
+				temp = "";
+			}
+			temp += line;
+			temp += "\n";
+		}
+		messages.add(temp.substring(0, temp.lastIndexOf("\n")));
+		return messages;
+	}
+
+	@Override
+	public void handle(MessageReceivedEvent e, String[] parts) {
+		e.getAuthor().openPrivateChannel().queue(channel -> sendOrder.forEach(m -> channel.sendMessage(m).queue()));
+	}
 
 }
